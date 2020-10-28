d6af24cb64c043483c1326e1344f3d0c6fa5c5b4
ag <ag@whiznets.com>
2020-10-27 22:59:25 -0700

    a

--- popup.js
+++ popup.js
@@ -12,11 +12,26 @@ chrome.storage.sync.get('color', function(data) {
 });
 
 changeColor.onclick = function(element) {
-  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     let url = tabs[0].url;
     console.log(url);
-    console.log("hello");
-    console.log("test branch");
+
+    fetch(url).then(function(html)
+    {
+        html.text().then(function(text) 
+        {
+            console.log(text);
+            let re = new RegExp('<link rel="canonical".*', 'g');
+            let link   = text.match(re)[0];
+            link = link.split(" ")[2].split("/")[3];
+            console.log(link);
+        });
+      
+        
+        
+    });
+    
+   
     // use url here inside the callback because it's asynchronous!
   });
   // let color = element.target.value;
@@ -26,4 +41,10 @@ changeColor.onclick = function(element) {
   //       {code: 'document.body.style.backgroundColor = "' + color + '";'}, function() {
   //       });
   // });
+};
+
+async function test(){
+    const response = await fetch(url);
+    const text = await response.text();
+ 
 };
