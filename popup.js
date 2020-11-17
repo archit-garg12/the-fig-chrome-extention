0ab2e59c0f549a9c9d0670698ed0e9109ed3adb4
ag <ag@whiznets.com>
2020-11-16 23:25:00 -0800

    figscore get

--- popup.js
+++ popup.js
@@ -277,7 +277,7 @@ function stemTags(tags, stemmedSource, callback) {
 		  console.log(finalList)
           callback(finalList)
       } else {
-          console.log("hoe")
+          console.log("eoh")
       }
 
   };
